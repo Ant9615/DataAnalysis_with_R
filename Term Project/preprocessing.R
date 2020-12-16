657aa5a24f551df3725d17bdc235891ac40ec9c6
Gu Gyeongmin <rudals901@gmail.com>
2020-12-17 01:11:59 +0900

    DA_ISE_TP 0.1

---
+++ Term Project/preprocessing.R
@@ -0,0 +1,26 @@
+flare78$Class <- as.factor(flare78$Class)
+flare78$LSP <- as.factor(flare78$LSP)
+flare78$SD <- as.factor(flare78$SD)
+
+"""
+ flare69$ Class      Factor 
+ flare69$ LSP        Factor 
+ flare69$ SD         Factor 
+ flare69$ Activity   num  
+ flare69$ Evoluation num  
+ flare69$ Activity24 num  
+ flare69$ HC         num  
+ flare69$ RHC        num  
+ flare69$ Area       num  
+ flare69$ ALS        num  
+ flare69$ C_class    num  
+ flare69$ M_class    num  
+ flare69$ X_class    num  
+
+ Class, LSP, SD, Activity, Evoluation, Activity24, HC, RHC, Area, ALS, C_class, M_class, X_class
+
+flare69$Class, flare69$LSP, flare69$SD, flare69$Activity, flare69$Evoluation, flare69$Activity24, flare69$HC, flare69$RHC, flare69$Area, flare69$ALS, flare69$C_class, flare69$M_class, flare69$X_class
+
+flare78$Class, flare78$LSP, flare78$SD, flare78$Activity, flare78$Evoluation, flare78$Activity24, flare78$HC, flare78$RHC, flare78$Area, flare78$ALS, flare78$C_class, flare78$M_class, flare78$X_class
+
+ """
